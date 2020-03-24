ea8c9b939bee2b219926af5db0f6ac13417aa232
Shibani Das <53535402+Sphinx161@users.noreply.github.com>
2020-03-24 12:14:06 +0530

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -30,7 +30,7 @@ class MacChanger:
     @staticmethod
     def get_current_mac(interface):
         ifconfig_result = subprocess.check_output(["ifconfig", interface])
-        mac_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+        mac_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
         if mac_search_result:
             return mac_search_result.group(0)
         else:
